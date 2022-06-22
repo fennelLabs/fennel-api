3b7e9e312537a9952c37fdd9458a100029a1b529
Sean Batzel <romulus108@protonmail.com>
2022-06-22 16:29:34 -0400

    Made on-chain identity and fingerprint unique.

--- main/models.py
+++ main/models.py
@@ -18,7 +18,7 @@ class Message(models.Model):
 
 
 class Identity(models.Model):
-    on_chain_identity_number = models.IntegerField()
+    on_chain_identity_number = models.IntegerField(unique=True)
     fingerprint = models.CharField(max_length=256)
     public_key = models.CharField(max_length=2048)
     shared_secret_key = models.CharField(max_length=256)
