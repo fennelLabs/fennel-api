7639847c3ce9d83538cf60c2b06b1cb7b0c1290d
Sean Batzel <romulus108@protonmail.com>
2022-07-01 14:22:26 -0400

    Removing pagination from our API responses - for now.

--- fennelapi/settings.py
+++ fennelapi/settings.py
@@ -133,8 +133,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    # "PAGE_SIZE": 10,
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
 }
 
