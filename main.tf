6a191511a93189b6aabf100ded80d6a3ab27b534
Ed Hertzog <edhertzog@gmail.com>
2023-04-21 07:08:45 -0400

    Assign static IP, open correct ports

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
 module "gce-container" {
   source = "terraform-google-modules/container-vm/google"
-  version = "~> 2.0"  # Upgrade the version if necessary.
+  version = "~> 2.0" 
 
   container = {
-    #image = "https://us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-api:latest"
     image = "ubuntu-os-cloud/ubuntu-2004-lts"
   }
+}
+
+resource "google_compute_address" "fennel-api-ip" {
+  name = "fennel-api-ip"
 }
 
 resource "google_compute_instance" "fennel-api" {
@@ -19,29 +22,32 @@ resource "google_compute_instance" "fennel-api" {
     }
   }
 
-  #metadata_startup_script = "echo Hello, World!"
   metadata_startup_script = <<EOF
     #!/bin/bash
     apt-get update
     apt-get install -y docker.io
     gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
     docker pull us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-api:latest
-    docker run -dit -p 8080:8080 --name fennel-api us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-api:latest
+    docker run -dit -p 1234:1234 --name fennel-api us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-api:latest
   EOF  
-  
-  #"#!/bin/bash\ndocker run us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-api:latest"
   
   network_interface {
     network = "default"
-    access_config {}
+    access_config {
+      nat_ip = google_compute_address.fennel-api-ip.address
+    }
   }
 
- 
  metadata = {
     # Required metadata key.
     gce-container-declaration = module.gce-container.metadata_value
     google-logging-enabled    = "true"
     google-monitoring-enabled = "true"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["1234"]
   }
  
   service_account {
