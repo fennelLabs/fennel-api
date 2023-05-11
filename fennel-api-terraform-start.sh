268bca4283af7e7714b51105e925283bc7f35b8b
Ed Hertzog <edhertzog@gmail.com>
2023-05-11 19:48:27 -0400

    Expose external port

--- fennel-api-terraform-start.sh
+++ fennel-api-terraform-start.sh
@@ -2,7 +2,7 @@
     sudo apt-get update
     sudo apt-get install -y docker.io
     gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
-    docker run -dit -p 1234:1234 --name fennel-api us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-api:latest
+    docker run -dit -p 80:1234 --name fennel-api us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-api:latest
     docker exec -it fennel-api sh
     cd /opt/app
     ./build-dev.sh
