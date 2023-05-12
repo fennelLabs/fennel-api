bd3f000a6c926019ccd946a55293e4f35b3f95f8
Ed Hertzog <edhertzog@gmail.com>
2023-05-11 20:46:49 -0400

    Terraform working (#22)
    * Test
    
    * Change start-up script

--- fennel-api-terraform-start.sh
+++ fennel-api-terraform-start.sh
@@ -3,6 +3,4 @@
     sudo apt-get install -y docker.io
     gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
     docker run -dit -p 80:1234 --name fennel-api us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-api:latest
-    docker exec -it fennel-api sh
-    cd /opt/app
-    ./build-dev.sh
+    docker exec -it fennel-api /opt/app/build-dev.sh
