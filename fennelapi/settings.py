2d0a11b1f6bba6d13f180d4899a50662e36d3fc7
Sean Batzel <romulus108@protonmail.com>
2023-06-11 19:27:37 -0400

    Reconfigured for deployment.

--- fennelapi/settings.py
+++ fennelapi/settings.py
@@ -81,12 +81,24 @@ WSGI_APPLICATION = "fennelapi.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if "POSTGRES_USER" in os.environ:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.environ["POSTGRES_DB"],
+            "USER": os.environ["POSTGRES_USER"],
+            "PASSWORD": os.environ["POSTGRES_PASS"],
+            "HOST": os.environ["POSTGRES_NAME"],
+            "PORT": 5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
