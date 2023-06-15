660094c72c2a29f955d23aaebf563df41fa1c12a
Sean Batzel <romulus108@protonmail.com>
2023-06-15 13:09:40 -0400

    Recompiling with corrected settings.

--- fennel-api-terraform-start.sh
+++ fennel-api-terraform-start.sh
@@ -14,7 +14,7 @@ export SECRET_KEY=$(gsutil cat gs://whiteflag-0-admin/api-secret-key.sh)
 export POSTGRES_DB=$(gsutil cat gs://whiteflag-0-admin/postgres_db.sh)
 export POSTGRES_USER=postgres
 export POSTGRES_PASS=$(gsutil cat gs://whiteflag-0-admin/postgres_pass.sh)
-export POSTGRES_NAME=fennel-api
+export POSTGRES_NAME=fennel_api
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
-docker run -dit -p 80:1234 --name fennel-api us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-api:latest
+docker run -dit -e DEBUG -e ADMIN_NAME -e ADMIN_EMAIL -e EMAIL_HOST -e EMAIL_PORT -e EMAIL_USERNAME -e EMAIL_PASSWORD -e DEFAULT_FROM_EMAIL -e SERVER_EMAIL -e SECRET_KEY -e POSTGRES_DB -e POSTGRES_USER -e POSTGRES_PASS -e POSTGRES_NAME -e FENNEL_CLI_IP -e FENNEL_API_IP -e FENNEL_KEYSERVER_IP -e FENNEL_SUBSERVICE_IP -p 80:1234 --name fennel-api us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-api:latest
 docker exec -it fennel-api /opt/app/build-dev.sh
